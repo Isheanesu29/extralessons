42708813a8ebdf7f473c1fbdb7ee7231474ced58
Isheanesu Murwira <imurwira@yahoo.com>
2021-06-10 22:08:30 +0200

    added slicing and index files

---
+++ index.py
@@ -0,0 +1,6 @@
+# locating an index of a string
+
+message1 ='Isheanesu first python class'
+message2 ="Isheanesu's first python class"
+
+print(f"index element is: {message1[5]} {message2}")
---
+++ slising.py
@@ -0,0 +1,7 @@
+## we are slicing the message
+## if the index you put is out of range its going to tell you that its out of range
+
+message1 ='Isheanesu first python class'
+message2 ="Isheanesu's first python class"
+
+print(message1[5:40])
