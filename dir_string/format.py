752ea89daf37f97206a662ea83d12e2f79427ca3
Isheanesu Murwira <imurwira@yahoo.com>
2021-06-15 21:22:47 +0200

    added list

---
+++ dir_string/format.py
@@ -0,0 +1,21 @@
+# place holder {} funtions with a f in the begining without the f it does not know what to call.
+# use dir to see other  extenstion funtions that will help you compute or do what ever you want to do on string
+
+import datetime as date
+
+
+message1 ='Isheanesu second'
+message2 ="python class"
+time = date.datetime.now()
+
+# print(f'This is {message1} {message2}')
+
+# print(f'The time is {time}')
+
+# print("Isheanesu first python class")
+
+# print(dir(message1))
+
+print(help(str))
+
+
